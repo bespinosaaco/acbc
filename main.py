fe9238fe33c73adb1f95cd9cb967615b91cd7c88
brian <brianespinosaacosta@gmail.com>
2025-01-24 11:57:39 -0330

    csv edit 1

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import pandas as pd
 df = pd.read_csv('master.csv',header=0)
 
 df.iloc[-1,0] = 'acbc0144'
-print(df.iloc[-1,0])
+# print(df.iloc[-1,0])
 print(df)
+
+df.to_csv('master.csv',index=False)
